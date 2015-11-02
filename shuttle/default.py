ceed01a91b16236fe13fc79913e197a2747e28af
Henning Peters <pete@dexterslab.de>
2015-11-02 16:41:35 +0100

    fix

--- shuttle/default.py
+++ shuttle/default.py
@@ -1,7 +1,9 @@
+import os
+
 # cli/param defaults
-install_repository_url = 'http://shuttle/index'
-upload_repository_url = 'http://shuttle/index'
-update_repository_url = 'http://shuttle/index'
+install_repository_url = os.environ['REPOSITORY_URL']
+upload_repository_url = os.environ['REPOSITORY_URL']
+update_repository_url = os.environ['REPOSITORY_URL']
 list_meta = False
 list_package_string = '*'
 build_package_path = '.'
