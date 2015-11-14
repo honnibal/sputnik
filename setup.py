81fefd3a1e33ea23975395a2c20e61a3aa8440ef
Henning Peters <pete@dexterslab.de>
2015-11-14 07:54:43 +0100

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sputnik',
-    version='0.1.1',
+    version='0.2.0',
     description='Sputnik data package manager library',
     url='https://github.com/henningpeters/sputnik',
     author='Henning Peters',
