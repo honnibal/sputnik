ab3edac3c1d1a5dc7d23ac5f45ce2db6f7411cea
Henning Peters <pete@dexterslab.de>
2015-12-28 10:52:53 +0100

    bump version

--- sputnik/__about__.py
+++ sputnik/__about__.py
@@ -4,7 +4,7 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __title__ = 'sputnik'
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 __summary__ = 'Sputnik data package manager library'
 __uri__ = 'https://github.com/henningpeters/sputnik'
 __author__ = 'Henning Peters'
